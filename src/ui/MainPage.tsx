571f2ed407997b89fd4b8d65fb04a9718d33819e
Aleksey Astashonok <lextapir191919@gmail.com>
2025-11-29 13:42:41 +0300

    delete h1

--- src/ui/MainPage.tsx
+++ src/ui/MainPage.tsx
@@ -8,7 +8,6 @@ export function MainPage() {
   return (
     <div>
       <div style={{ display: "flex", gap: "40px" }}>
-        <h1>Musicfun Player</h1>
         <TracksList
           selectedTrackId={trackId}
           onTrackSelect={handleTrackSelect}
